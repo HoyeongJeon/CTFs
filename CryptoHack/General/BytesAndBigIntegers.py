a63319e8300eff82aeed9e3e9b9d2cb96b1b06e9
HoyeongJeon <hoyeong0302@gmail.com>
2023-03-10 19:04:58 +0900

    feat: XOR Starter

--- CryptoHack/General/BytesAndBigIntegers.py
+++
@@ -1,5 +0,0 @@
-import Crypto
-from Crypto.Util.number import *
-encoded_int = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
-
-print(long_to_bytes(encoded_int))
---
+++ CryptoHack/General/XOR/XORStarter.py
@@ -0,0 +1,1 @@
+print(''.join([chr(ord(c) ^ 13) for c in 'label']))
