ed08f30b8174b14cd4430a38dfda912cc4b724c3
HoyeongJeon <hoyeong0302@gmail.com>
2023-03-13 22:00:13 +0900

    chore: LemurXOR.py 불필요한 line 제거

--- CryptoHack/General/XOR/LemurXOR.py
+++ CryptoHack/General/XOR/LemurXOR.py
@@ -1,20 +1,3 @@
-#I've hidden two cool images by XOR with the same secret key so you can't see them!
-
-'''
-Lemur ^ S
-Flag ^ S
-
-'''
-# import numpy as np
-# import cv2
-
-# lemur_png = './lemur.png'
-# flag_png = './flag.png'
-
-# print(type(lemur_png))
-# xored = cv2.bitwise_xor(lemur_png, flag_png)
-# cv2.imshow("xored",xored)
-
 from PIL import Image
 
 # Load the two PNG files
